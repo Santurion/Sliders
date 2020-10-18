b524611abc31f93f594a922ca416d9d96a1da7ea
Aleksander Shuplyk <santurionley@rambler.ru>
2020-10-18 16:40:15 +0300

    1

--- Sliders/ViewController.swift
+++ Sliders/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultView: UIView!
+    
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultView.layer.cornerRadius = 10
+     
     }
 
 
